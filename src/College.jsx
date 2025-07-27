88117b51205453ad9ef1b7daecdaa1bfd374031c
shivani <shivaniw69@gmail.com>
2025-07-27 16:58:11 +0530

    aollege

--- src/College.jsx
+++ src/College.jsx
@@ -11,3 +11,16 @@
 // }
 //  export default College;
 
+
+
+// const College=(props)=>{
+//     return(
+//         <>
+//         <h1>my College: {props.nm}</h1>
+//         <h1>seats: {props.st}</h1>
+//         <h1> my fees: {props.fs}</h1>
+//         </>
+//     )
+// }
+//  export default College;
+
