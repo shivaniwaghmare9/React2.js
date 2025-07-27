3552c5dac95776c9a3319a52ac6a858490fe1157
shivani <shivaniw69@gmail.com>
2025-07-27 17:04:51 +0530

    student

--- src/Student.jsx
+++ src/Student.jsx
@@ -11,3 +11,17 @@
 // }
 
 // export default Student;
+
+
+
+// const Student=(props)=>{
+//     return(
+//         <>
+//         <h1>Hello i am: {props.nm}</h1>
+//         <h1>my city: {props.city}</h1>
+//         <h1> I learn: {props.sub}</h1>
+//         </>
+//     )
+// }
+
+// export default Student;
