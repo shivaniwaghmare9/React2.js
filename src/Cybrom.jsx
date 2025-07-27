d1738898f47df2ee12ea1d968dfe66b2de894790
shivani <shivaniw69@gmail.com>
2025-07-27 16:59:40 +0530

    adp

--- src/Cybrom.jsx
+++ src/Cybrom.jsx
@@ -12,17 +12,3 @@
 // }
 //  export default Cybrom;
 
-
-
-
-// const Cybrom=(props)=>{
-//     return(
-//         <>
-//         <h1>my College: {props.nm}</h1>
-//         <h1>seats: {props.st}</h1>
-//         <h1> my fees: {props.fs}</h1>
-//         </>
-//     )
-// }
-//  export default Cybrom;
-
