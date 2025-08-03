3001d67abc9d55ff471cfe1b831d95d008fdfada
shivani <shivaniw69@gmail.com>
2025-08-03 23:16:57 +0530

    about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,25 +2,11 @@
 
 
 
-// const About=()=>{
-//     return(
-//         <>
-//         <h1>About Us!!!</h1>
-//         </>
-//     )
-
-// }
-// export default About;
-
-
-
-
 const About=()=>{
     return(
         <>
         <h1>About Us!!!</h1>
         </>
     )
-    
 }
 export default About;
