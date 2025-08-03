3094c393e322d41b2836372011a74360de91f827
shivani <shivaniw69@gmail.com>
2025-08-03 23:17:52 +0530

    contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,15 +1,5 @@
 
 
-// const Contact=()=>{
-//     return(
-//         <>
-//         <h1>Contact Us!!!</h1>
-//         </>
-//     )
-// }
-// export default Contact;
-
-
 const Contact=()=>{
     return(
         <>
