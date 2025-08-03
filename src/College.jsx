f213ac8f071508294dd2163a9f23e2c01f5410f4
shivani <shivaniw69@gmail.com>
2025-08-03 23:20:48 +0530

    cybrom

--- src/College.jsx
+++ src/College.jsx
@@ -1,19 +1,5 @@
 
 
-const College=(props)=>{
-    return(
-        <>
-        <h1>my College: {props.nm}</h1>
-        <h1>seats: {props.st}</h1>
-        <h1> my fees: {props.fs}</h1>
-        </>
-    )
-}
- export default College;
-
-
- 
-
 // const College=(props)=>{
 //     return(
 //         <>
