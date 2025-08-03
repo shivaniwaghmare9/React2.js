bdacbd7cc4f425aedf1ca9227061b733e749fe1c
shivani <shivaniw69@gmail.com>
2025-08-03 23:28:50 +0530

    not

--- src/Nofound.jsx
+++ src/Nofound.jsx
@@ -1,5 +1,15 @@
 
 
+// const Nofound=()=>{
+//     return(
+//         <>
+//         <h1>NO found !! 404-Not found page!!!</h1>
+//         </>
+//     )
+// }
+// export default Nofound;
+
+
 const Nofound=()=>{
     return(
         <>
