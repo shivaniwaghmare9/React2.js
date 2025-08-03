21a472f4bcd83e896f530d825c77c086388824fe
shivani <shivaniw69@gmail.com>
2025-08-03 23:27:53 +0530

    layout

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,5 +1,26 @@
 
 
+// import { Link,Outlet } from "react-router-dom";
+
+// const Layout=()=>{
+//     return(
+//         <>
+//     <Link to="home">Home</Link>
+//     <Link to="about">About Us</Link>
+//     <Link to="contact">Contact us</Link>
+
+//     <hr size="4" color="red"/>
+//    <Outlet/>
+
+//     <hr size="4" color="red"/>
+
+//     www.company.com
+//         </>
+//     )
+// }
+// export default Layout;
+
+
 import { Link,Outlet } from "react-router-dom";
 
 const Layout=()=>{
