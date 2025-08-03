794fb121d895189fe8b389ec99183861a304e039
shivani <shivaniw69@gmail.com>
2025-08-03 23:23:15 +0530

    disign

--- src/Empdesign.jsx
+++ src/Empdesign.jsx
@@ -1,5 +1,21 @@
 
 
+// const Design=(props)=>{
+//     return(
+//         <>
+//         <tr>
+//             <td>{props.nm}</td>
+//             <td>{props.emn}</td>
+//             <td>{props.desig}</td>
+//             <td>{props.sal}</td>
+//         </tr>
+//         </>
+
+//     )
+// }
+// export default Design;
+
+
 const Design=(props)=>{
     return(
         <>
