9929a5144f6bf90764903aa2e5dc6cc86529a8b0
shivani <shivaniw69@gmail.com>
2025-08-03 23:21:17 +0530

    clg

--- src/Cybrom.jsx
+++ src/Cybrom.jsx
@@ -12,3 +12,17 @@
 // }
 //  export default Cybrom;
 
+
+
+
+// const Cybrom=(props)=>{
+//     return(
+//         <>
+//         <h1>my College: {props.nm}</h1>
+//         <h1>seats: {props.st}</h1>
+//         <h1> my fees: {props.fs}</h1>
+//         </>
+//     )
+// }
+//  export default Cybrom;
+
